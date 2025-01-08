7caedf07d36099d46026658262a03be93602e779
KAVERAPPA <114868311+ka25van@users.noreply.github.com>
2025-01-08 11:17:33 +0530

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 // add the "use client" directive at the top of your file. This will ensure that the file is treated as a client component in Next.js.
-import Register from "./pages/register"
-import Login from "./pages/login";
 import Dashboard from "./pages/dashboard"
 
 export default function Home() {
   return (
     <div>
-      {/* <Register/>
-      <Login/> */}
       <Dashboard/>
     </div>
   );
